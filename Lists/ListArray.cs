62a92bfb406f065b624abdf6e35b46cc2727e982
8m3xl <8m3xlt@gmail.com>
2022-09-15 14:17:59 +0100

    Add project files.

---
+++ Lists/ListArray.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+namespace Lists
+{
+    public class ListArray
+    {
+        private Item[] arrayOfItems;
+        private int numItems;
+        private int capacity;
+
+        public int Count
+        {
+            get { return numItems; }
+        }
+
+        public ListArray() : this(capacity: 100)
+        {
+        }
+
+        public ListArray(int capacity)
+        {
+            arrayOfItems = new Item[capacity];
+            this.capacity = capacity;
+            numItems = 0;
+        }
+
+        public void Add(Item item)
+        {
+            if (!Full())
+            {
+                arrayOfItems[numItems] = item;
+                numItems++;
+            }
+        }
+
+        public bool Full()
+        {
+            return numItems >= arrayOfItems.Length;
+        }
+
+        public bool Empty()
+        {
+            return numItems == 0;
+        }
+
+        public bool Remove(Item item)
+        {
+            // Removes all items that match item. 
+            // Returns true if there was any.
+            int location = 0;
+            bool result = false;
+            while (location < numItems)
+            {
+                if (arrayOfItems[location].ProductName == item.ProductName)
+                {
+                    arrayOfItems[location] = arrayOfItems[numItems - 1];
+                    numItems--;
+                    location--; // to ensure item moved up is checked
+                    result = true;
+                }
+                location++;
+            }
+            return result;
+        }
+
+        public void RemoveAt(int index)
+        {
+            if (numItems >= index)
+            {
+                arrayOfItems[index] = arrayOfItems[numItems];
+                numItems--;
+            }
+        }
+
+        public Item Retrieve(int index)
+        {
+            if (index <= numItems)
+            {
+                return arrayOfItems[index];
+            }
+            else
+            {
+                throw new InvalidOperationException("Index out of boundaries");
+            }
+        }
+
+        public bool Found(Item item, out int position)
+        {
+            for (int i = 0; i < numItems; i++)
+            {
+                if (arrayOfItems[i].ProductName == item.ProductName)
+                {
+                    position = i;
+                    return true;
+                }
+            }
+            position = -1;
+            return false;
+        }
+    }
+}
---
+++ Lists/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Testing the class
+            var myList = new ListArray();
+            myList.Add(new Item { ProductName = "carrots", Quantity = 1 });
+            myList.Add(new Item { ProductName = "milk", Quantity = 1 });
+            myList.Add(new Item { ProductName = "apples", Quantity = 1 });
+            myList.Add(new Item { ProductName = "biscuits", Quantity = 2 });
+            myList.Add(new Item { ProductName = "carrots", Quantity = 1 });
+
+            // List the items in the list
+
+            for (int i = 0; i < myList.Count; i++)
+            {
+                Console.WriteLine(i);
+                Console.WriteLine(myList.Retrieve(i).ProductName);
+            }
+
+            // Remove carrots
+
+            myList.Remove(new Item { ProductName = "carrots" });
+
+            // List the items in the list again
+            Console.WriteLine("\n");
+            for (int i = 0; i < myList.Count; i++)
+            {
+                Console.WriteLine(i);
+                Console.WriteLine(myList.Retrieve(i).ProductName);
+            }
+
+            // Ask the user for an item and output its position in the list
+
+            Console.WriteLine("\nItem name: ");
+            string item_name = Console.ReadLine();
+
+            Console.WriteLine("Item quantity: ");
+            int item_quant = int.Parse(Console.ReadLine());
+
+            Item item = new Item { ProductName = item_name, Quantity = item_quant };
+
+            int position;
+            Console.WriteLine(myList.Found(item, out position));
+            Console.WriteLine(position);
+        }
+    }
+}
