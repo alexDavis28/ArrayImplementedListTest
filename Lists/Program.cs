ada57441cbf3db56ff8d77fd4de723d4abc963fd
8m3xl <8m3xlt@gmail.com>
2022-09-18 13:18:18 +0100

    Use generic lists

--- Lists/Program.cs
+++ Lists/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Lists
 {
@@ -7,7 +8,7 @@ namespace Lists
         static void Main(string[] args)
         {
             // Testing the class
-            var myList = new ListArray();
+            List<Item> myList = new List<Item> { };
             myList.Add(new Item { ProductName = "carrots", Quantity = 1 });
             myList.Add(new Item { ProductName = "biscuits", Quantity = 2 });
             myList.Add(new Item { ProductName = "apples", Quantity = 1 });
@@ -23,34 +24,34 @@ namespace Lists
             for (int i = 0; i < myList.Count; i++)
             {
                 Console.WriteLine(i);
-                Console.WriteLine(myList.Retrieve(i).ProductName);
+                Console.WriteLine(myList[i].ProductName);
             }
 
             // Remove carrots
 
-            myList.Remove(new Item { ProductName = "carrots" });
+            myList.RemoveAll(x => x.ProductName == "carrots");
 
             // List the items in the list again
             Console.WriteLine("\n");
             for (int i = 0; i < myList.Count; i++)
             {
                 Console.WriteLine(i);
-                Console.WriteLine(myList.Retrieve(i).ProductName);
+                Console.WriteLine(myList[i].ProductName);
             }
 
             // Test adding new spaces
-            Console.WriteLine("\nCurrent spaces: " + myList.Length()); // Expected: 100
+            Console.WriteLine("\nCurrent spaces: " + myList.Count); // Expected: 100
             Console.WriteLine("Adding 100 items");
             for (int i = 0; i < 100; i++)
             {
                 myList.Add(new Item { ProductName = "egg", Quantity = 1 });
             }
-            Console.WriteLine("Current spaces: " + myList.Length()); // Expected: 200
+            Console.WriteLine("Current spaces: " + myList.Count); // Expected: 200
 
             // Test removing unused spaces
             Console.WriteLine("Removing 100 items");
             myList.Remove(new Item { ProductName = "egg" });
-            Console.WriteLine("Current spaces: " + myList.Length()); // Expected: 100
+            Console.WriteLine("Current spaces: " + myList.Count); // Expected: 100
 
             // Ask the user for an item and output its position in the list
 
@@ -62,8 +63,7 @@ namespace Lists
 
             Item item = new Item { ProductName = item_name, Quantity = item_quant };
 
-            int position;
-            Console.WriteLine(myList.Found(item, out position));
+            int position = myList.IndexOf(item);
             Console.WriteLine(position);
         }
     }
